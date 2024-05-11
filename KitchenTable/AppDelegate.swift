4dceea7a062c3618cf3f41b2797e6488d8eeb5f7
Anders Hovmöller <anders.hovmoller@dryft.se>
2024-05-11 19:06:12 +0200

    .

--- KitchenTable/AppDelegate.swift
+++ KitchenTable/AppDelegate.swift
@@ -50,24 +50,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var pm_label: NSTextField!
     @IBOutlet weak var weekday_label: NSTextField!
     @IBOutlet weak var date_label: NSTextField!
-    @IBOutlet weak var image: NSImageCell!
     @IBOutlet weak var errors: NSTextField!
+    @IBOutlet weak var leftImage: NSImageView!
+    @IBOutlet weak var rightImage: NSImageView!
     var store = EKEventStore()
     let timeFormatter = DateFormatter()
     var lastChanged: Date = Date()
+    var dataChanged: Date = Date()
     var pngData: Data?
     var timer: Timer?
     var timeOfWeatherData = Date.distantPast
     var task: URLSessionDataTask?
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        view.layer?.backgroundColor = .black
+        view.layer?.backgroundColor = .white
+        
+        dateUpdater()
         
         timer = Timer.scheduledTimer(
             withTimeInterval: 5,
             repeats: true,
             block: {_ in
-                self.updateDisplay()
+                self.dateUpdater()
             }
         )
         
@@ -94,7 +98,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 if granted {
                     DispatchQueue.main.async {
                         self.readCalendar()
-                        self.updateDisplay()
                     }
                 }
                 else {
@@ -106,7 +109,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             store.requestAccess(to: .event, completion: { granted,_ in
                 if granted {
                     self.readCalendar()
-                    self.updateDisplay()
                 }
                 else {
                     self.setError("Error getting calendar access")
@@ -116,9 +118,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @MainActor
-    func updateDisplay() {
-        let prev_changed = lastChanged
-        readCalendar()
+    func dateUpdater() {
         let now = Date()
         let weekday_name_by_number = [
             2: "Måndag",
@@ -131,39 +131,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         ]
         
         let new_weekday_label = weekday_name_by_number[now.weekday]!
-        if weekday_label.stringValue != new_weekday_label {
-            weekday_label.stringValue = new_weekday_label
-            lastChanged = Date()
+        if self.weekday_label.stringValue != new_weekday_label {
+            self.weekday_label.stringValue = new_weekday_label
+            self.dataChanged = Date()
         }
         let new_dateLabel = "\(now.day)"
-        if date_label.stringValue != new_dateLabel {
-            date_label.stringValue = new_dateLabel
-            lastChanged = Date()
+        if self.date_label.stringValue != new_dateLabel {
+            self.date_label.stringValue = new_dateLabel
+            self.dataChanged = Date()
+        }
+        self.updateDisplay()
+    }
+    
+    @MainActor
+    func updateDisplay() {
+        if self.dataChanged == self.lastChanged {
+            return
         }
         
-        if lastChanged != prev_changed {
-            NSLog("new image")
+        NSLog("new image")
 
-            guard let imgData = view.bitmapImageRepForCachingDisplay(in: view.bounds) else {
-                assert(false)
-                return
-            }
-            
-            view.cacheDisplay(in: view.bounds, to: imgData)
-            view.unlockFocus()
-            pngData = imgData.representation(using: .png, properties: [:])
-            self.lastChanged = Date()
-            /*
-            do {
-                let path = URL.init(fileURLWithPath: "output.png")
-                NSLog(path.absoluteString)
-                try pngData!.write(to: path, options: .atomic)
-            }
-            catch {
-                assert(false)
-            }
-             */
+        guard let imgData = view.bitmapImageRepForCachingDisplay(in: view.bounds) else {
+            assert(false)
+            return
         }
+        
+        view.cacheDisplay(in: view.bounds, to: imgData)
+        pngData = imgData.representation(using: .png, properties: [:])
+        self.lastChanged = dataChanged
+        /*
+        do {
+            let path = URL.init(fileURLWithPath: "output.png")
+            NSLog(path.absoluteString)
+            try pngData!.write(to: path, options: .atomic)
+        }
+        catch {
+            assert(false)
+        }
+         */
     }
     
     @MainActor
@@ -190,19 +195,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let new_am_label = am_events.joined(separator: "\n")
         if new_am_label != self.am_label.stringValue {
-            lastChanged = Date()
+            dataChanged = Date()
         }
         self.am_label.stringValue = new_am_label
         
         let new_pm_label = pm_events.joined(separator: "\n")
         if new_pm_label != self.pm_label.stringValue {
-            lastChanged = Date()
+            dataChanged = Date()
         }
         self.pm_label.stringValue = new_pm_label
+        self.updateDisplay()
     }
     
     func readWeather() {
-        let s = "https://api.open-meteo.com/v1/forecast?latitude=\(loc.coordinate.latitude)&longitude=\(loc.coordinate.longitude)&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum,wind_speed_10m_max&forecast_days=1&timezone=Europe%2FBerlin"
+        let s = "https://api.open-meteo.com/v1/forecast?latitude=\(loc.coordinate.latitude)&longitude=\(loc.coordinate.longitude)&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum,wind_speed_10m_max,uv_index_max&forecast_days=1&timezone=Europe%2FBerlin"
         guard (Date().timeIntervalSince1970 - timeOfWeatherData.timeIntervalSince1970) > 60*60 else {  // don't update more than once an hour
             return
         }
@@ -239,58 +245,41 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                         decoder.dateDecodingStrategy = JSONDecoder.DateDecodingStrategy.secondsSince1970
                         let result = try decoder.decode(OMWeatherData.self, from: data)
                         NSLog("Parsed!")
-                        
-                        let weatherSymbol = result.daily.weather_code[0]
-
-                        NSLog("Parsed! 2")
-
-                        /*
-                         0              Clear sky
-                         1, 2, 3        Mainly clear, partly cloudy, and overcast
-                         45, 48         Fog and depositing rime fog
-                         51, 53, 55     Drizzle: Light, moderate, and dense intensity
-                         56, 57         Freezing Drizzle: Light and dense intensity
-                         61, 63, 65     Rain: Slight, moderate and heavy intensity
-                         66, 67         Freezing Rain: Light and heavy intensity
-                         71, 73, 75     Snow fall: Slight, moderate, and heavy intensity
-                         77             Snow grains
-                         80, 81, 82     Rain showers: Slight, moderate, and violent
-                         85, 86         Snow showers slight and heavy
-                         95 *           Thunderstorm: Slight or moderate
-                         96, 99 *       Thunderstorm with slight and heavy hail
-                         */
-                        
-                        let symbol = switch weatherSymbol {
-                        case 0:
-                            "sun.max"
-                        case 1:
-                            "sun.max"
-                        case 2:
-                            "cloud.sun"
-                        case 3:
-                            "cloud"
-                        case 51...67:
-                            "cloud.heavyrain"
-                        case 80...86:
-                            "cloud.heavyrain"
-                        case 95...99:
-                            "cloud.bolt"
-                        case 45...48:
-                            // fog
-                            "cloud.sun"
-                        default:
-                            "cloud.sun"
-                        }
-                        
-                        NSLog("Parsed! 3")
 
                         self.timeOfWeatherData = Date()
                         
-                        NSLog("Parsed! 4")
-
                         DispatchQueue.main.async {
                             NSLog("image set...")
-                            self.image.image = NSImage(named: symbol)!
+                            // Sun icon = needs sunscreen
+                            if result.daily.uv_index_max[0] >= 3 {
+                                if self.rightImage.image == nil {
+                                    self.rightImage.image = NSImage(systemSymbolName: "sun.max", variableValue: 0, accessibilityDescription: "")!
+                                    self.dataChanged = Date()
+                                }
+                            }
+                            else {
+                                if self.leftImage.image != nil {
+                                    self.rightImage.image = nil
+                                    self.dataChanged = Date()
+                                }
+                            }
+                            // Rain icon == needs rain gear
+                            if result.daily.rain_sum[0] >= 10 {
+                                if self.leftImage.image == nil {
+                                    self.leftImage.image = NSImage(systemSymbolName: "cloud.heavyrain", variableValue: 0, accessibilityDescription: "")!
+                                    self.dataChanged = Date()
+                                }
+                            }
+                            else {
+                                if self.leftImage.image != nil {
+                                    self.leftImage.image = nil
+                                    self.dataChanged = Date()
+                                }
+                            }
+                            
+                            while Date() == self.lastChanged {
+                                Thread.sleep(forTimeInterval: 1)
+                            }
                             NSLog("updateDisplay")
                             self.updateDisplay()
                         }
@@ -340,4 +329,5 @@ struct OMDaily : Decodable {
     let rain_sum: [Float]
     let snowfall_sum: [Float]
     let wind_speed_10m_max: [Float]
+    let uv_index_max: [Float]
 }
